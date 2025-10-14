4fcc39ec18b119c8fe9bafed9fa77d527e01d785
Gerard <ga.harkema@avans.nl>
2025-10-14 08:35:15 +0200

    20251014

--- amr_gazebo/launch/pioneer3dx_mazeworld.launch.py
+++ amr_gazebo/launch/pioneer3dx_mazeworld.launch.py
@@ -22,29 +22,23 @@ def launch_setup(context, *args, **kwargs):
     sim_pkg = get_package_share_directory('amr_gazebo')
 
     # === Launch arguments ===
-    urdf_gazebo_file = LaunchConfiguration('urdf_gazebo').perform(context)
-    urdf_rviz_file = LaunchConfiguration('urdf_rviz').perform(context)
+    urdf_file = LaunchConfiguration('urdf_gazebo').perform(context)
     world_file = LaunchConfiguration('world').perform(context)
     rviz_config_file = LaunchConfiguration('rviz_config').perform(context)
 
     # === Process the URDF/Xacro ===
-    if urdf_gazebo_file.endswith('.xacro'):
-        robot_gazebo_description = xacro.process_file(urdf_gazebo_file).toxml()
+    if urdf_file.endswith('.xacro'):
+        robot_description = xacro.process_file(urdf_file).toxml()
     else:
-        with open(urdf_gazebo_file, 'r') as f:
-            robot_gazebo_description = f.read()
-
-    if urdf_rviz_file.endswith('.xacro'):
-        robot_rviz_description = xacro.process_file(urdf_rviz_file).toxml()
-    else:
-        with open(urdf_rviz_file, 'r') as f:
-            robot_rviz_description = f.read()
+        with open(urdf_file, 'r') as f:
+            robot_description = f.read()
 
 
+    desc_pkg_gazebo = desc_pkg + "/.."
     # === Gazebo resource path ===
     gz_resource_path = SetEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
-        value=f"{desc_pkg}:{os.environ.get('GZ_SIM_RESOURCE_PATH','')}"
+        value=f"{desc_pkg_gazebo}:{os.environ.get('GZ_SIM_RESOURCE_PATH','')}"
     )
 
     # === Launch Gazebo Ignition ===
@@ -55,37 +49,36 @@ def launch_setup(context, *args, **kwargs):
         launch_arguments={'gz_args': f'-r -v 4 {world_file}'}.items(),
     )
 
-    # === Robot State Publisher ===
-    robot_state_publisher_rviz = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher_rviz',
-        output='screen',
-        parameters=[
-            {'use_sim_time': True},
-            {'robot_description': robot_rviz_description},
-        ],
-    )
+
 
     # === Robot State Publisher ===
-    robot_state_publisher_gazebo = Node(
+    robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        name='robot_state_publisher_gazebo',
+        name='robot_state_publisher',
         output='screen',
         parameters=[
             {'use_sim_time': True},
-            {'robot_description': robot_gazebo_description},
+            {'robot_description': robot_description},
         ],
     )
 
     # === Spawn robot in Gazebo ===
-    spawn_robot = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=['-name', 'pioneer3dx', '-topic', 'robot_gazebo_description'],
-        output='screen',
-    )
+    if 0:
+        spawn_robot = Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=['-name', 'pioneer3dx', '-topic', 'robot_description'],
+            output='screen',
+        )
+    else:
+        sdf_robot_description = os.path.join(sim_pkg, 'models', 'robots', 'pioneer3dx.sdf')
+        spawn_robot = Node(
+            package='ros_gz_sim',
+            executable='create',
+            arguments=['-name', 'pioneer3dx', '-file', sdf_robot_description],
+            output='screen',
+        )
 
     # === ROS–Gazebo bridge for cmd_vel ===
     ros_gz_bridge = Node(
@@ -111,8 +104,7 @@ def launch_setup(context, *args, **kwargs):
     return [
         gz_resource_path,
         gazebo_launch,
-        robot_state_publisher_rviz,
-        robot_state_publisher_gazebo,
+        robot_state_publisher,
         spawn_robot,
         ros_gz_bridge,
         rviz_node,
@@ -131,17 +123,12 @@ def generate_launch_description():
         description='Path to Gazebo Ignition world file',
     )
 
-    urdf_gazebo_arg = DeclareLaunchArgument(
+    urdf_arg = DeclareLaunchArgument(
         'urdf_gazebo',
-        default_value=os.path.join(desc_pkg, 'urdf', 'pioneer3dx_gazebo.urdf.xacro'),
-        description='Path to robot URDF or Xacro file',
-    )
-
-    urdf_rviz_arg = DeclareLaunchArgument(
-        'urdf_rviz',
         default_value=os.path.join(desc_pkg, 'urdf', 'pioneer3dx.urdf.xacro'),
         description='Path to robot URDF or Xacro file',
     )
+
 
     rviz_arg = DeclareLaunchArgument(
         'rviz_config',
@@ -151,8 +138,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         world_arg,
-        urdf_gazebo_arg,
-        urdf_rviz_arg,
+        urdf_arg,
         rviz_arg,
         OpaqueFunction(function=launch_setup),
     ])
